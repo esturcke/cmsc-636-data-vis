82a3da1c2f485e0e925da3b761fe40268c2c8dc3
Erik J. Sturcke <erik@sturcke.co>
2016-09-12 01:46:43 -0400

    Shorten line

--- assignment-1/vis-1/vis.js
+++ assignment-1/vis-1/vis.js
@@ -1,4 +1,6 @@
-d3.json("https://raw.githubusercontent.com/esturcke/cmsc-636-data-vis/master/assignment-1/data/ehr.json", (error, patients) => {
+const DATA = "https://raw.githubusercontent.com/esturcke/cmsc-636-data-vis/master/assignment-1/data/ehr.json"
+
+d3.json(DATA, (error, patients) => {
   if (error) throw error
   console.info(patients)
 })
