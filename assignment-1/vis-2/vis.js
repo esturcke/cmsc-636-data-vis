dacc97b95cef617811d880f80a4f4a06538dfa2d
Erik J. Sturcke <erik@sturcke.co>
2016-09-14 15:31:04 -0400

    Add patient processor that will be used to enhance patient data

--- assignment-1/vis-2/vis.js
+++ assignment-1/vis-2/vis.js
@@ -6,6 +6,11 @@ const DATA = "./ehr.json"
 const AREA = [ 960, 500 ]
 const MARGIN = { top : 20, right : 20, bottom : 20, left : 20 }
 
+/**
+ * Process data for a patient adding derived fields
+ */
+const enhance = patient => patient
+
 // setup SVG area to draw on
 const svg = d3.select("body").append("svg")
   .attr("width", AREA[0])
@@ -18,6 +23,6 @@ d3.json(DATA, (error, patients) => {
 
   // join patients and create groups for each
   svg.selectAll(".patient")
-    .data(patients)
+    .data(patients.map(enhance))
     .enter().append("g").classed("patient", true)
 })
