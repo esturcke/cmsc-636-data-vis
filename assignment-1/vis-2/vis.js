8d927bda1d215e8368818e29570ebb078a9b8331
Erik J. Sturcke <erik@sturcke.co>
2016-09-15 01:04:16 -0400

    Draw dot for each encounter placed at [days from injury, age at injury]

--- assignment-1/vis-2/vis.js
+++ assignment-1/vis-2/vis.js
@@ -1,60 +1,58 @@
 "use strict"
 
-/* global d3, _ */
-
-// extract lodash functions
-const {
-  flow,
-} = _
+/* global d3  */
 
 // patient data source
 const data = "./ehr.json"
 
 // time conversion
-const msInDays  = t => t / 1000 / 3600 / 24
+const msInDays = t => t / 1000 / 3600 / 24
 
 // derived data
-const injuryAge = patient => patient.injury.age
+const injuryAge      = ({ patient }) => patient.injury.age
+const encounterAge   = ({ encounter }) => encounter.age
+const daysFromInjury = ({ patient : { injury }, encounter }) => msInDays(encounter.date - injury.date)
 
-// canvas dimensions
+// SVG area dimensions
 const area   = [960, 500]
 const margin = { top : 20, right : 20, bottom : 20, left : 20 }
 const height = area[1] - margin.top  - margin.bottom
 const width  = area[0] - margin.left - margin.right
 
 // setup x- and y-axis scales and value selectors
-const xScale = d3.scaleLinear().range([0, width]).nice()
-const xValue = ([{ injury }, encounter]) => msInDays(encounter.date - injury.date)
-const x      = flow(xValue, xScale)
-
-const yScale = d3.scaleLinear().range([height, 0]).nice()
-const y      = flow(injuryAge, yScale)
+const x = d3.scaleLinear().range([0, width]).nice()
+const y = d3.scaleLinear().range([height, 0]).nice()
 
 const translate = (x, y) => `translate(${x}, ${y})`
-
-/**
- * Process data for a patient adding derived fields
- */
-const enhance = patient => patient
+const to        = (xVal, yVal) => datum => translate(x(xVal(datum)), y(yVal(datum)))
 
 // setup SVG area to draw on
 const svg = d3.select("body").append("svg")
   .attr("width", area[0])
   .attr("height", area[1])
   .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .attr("transform", translate(margin.left, margin.top))
 
-d3.json(data, (error, patients) => {
+d3.json(data, (error, data) => {
   if (error) throw error
 
-  yScale.domain([d3.min(patients, injuryAge), d3.max(patients, injuryAge)])
+  x.domain([-300, 300])
+  y.domain([d3.min(data, injuryAge), d3.max(data, injuryAge)])
 
   // join patients and create groups for each
-  svg.selectAll(".patient")
-    .data(patients.map(enhance))
+  const patients = svg.selectAll(".patient")
+    .data(data)
     .enter()
     .append("g")
       .classed("patient", true)
-      .attr("transform", patient => translate(width / 2, y(patient)))
 
+  // join encounters and create a circle for each
+  const encounters = patients.selectAll(".encounter")
+    .data(({ patient }) => patient.encounters.map(encounter => ({ patient, encounter })))
+    .enter()
+    .append("circle")
+      .classed("encounter", true)
+      .attr("r", 2)
+      .attr("opacity", 0.5)
+      .attr("transform", to(daysFromInjury, injuryAge))
 })
