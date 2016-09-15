7405b03ab393d57af4f23bcfde184d380a1729eb
Erik J. Sturcke <erik@sturcke.co>
2016-09-14 22:28:07 -0400

    Translate the y position of patients to the age at the time of injury

--- assignment-1/vis-2/vis.js
+++ assignment-1/vis-2/vis.js
@@ -1,10 +1,36 @@
 "use strict"
 
-/* global d3 */
+/* global d3, _ */
 
-const DATA = "./ehr.json"
-const AREA = [ 960, 500 ]
-const MARGIN = { top : 20, right : 20, bottom : 20, left : 20 }
+// extract lodash functions
+const {
+  flow,
+} = _
+
+// patient data source
+const data = "./ehr.json"
+
+// time conversion
+const msInDays  = t => t / 1000 / 3600 / 24
+
+// derived data
+const injuryAge = patient => patient.injury.age
+
+// canvas dimensions
+const area   = [960, 500]
+const margin = { top : 20, right : 20, bottom : 20, left : 20 }
+const height = area[1] - margin.top  - margin.bottom
+const width  = area[0] - margin.left - margin.right
+
+// setup x- and y-axis scales and value selectors
+const xScale = d3.scaleLinear().range([0, width]).nice()
+const xValue = ([{ injury }, encounter]) => msInDays(encounter.date - injury.date)
+const x      = flow(xValue, xScale)
+
+const yScale = d3.scaleLinear().range([height, 0]).nice()
+const y      = flow(injuryAge, yScale)
+
+const translate = (x, y) => `translate(${x}, ${y})`
 
 /**
  * Process data for a patient adding derived fields
@@ -13,16 +39,22 @@ const enhance = patient => patient
 
 // setup SVG area to draw on
 const svg = d3.select("body").append("svg")
-  .attr("width", AREA[0])
-  .attr("height", AREA[1])
+  .attr("width", area[0])
+  .attr("height", area[1])
   .append("g")
-    .attr("transform", "translate(" + MARGIN.left + "," + MARGIN.top + ")")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-d3.json(DATA, (error, patients) => {
+d3.json(data, (error, patients) => {
   if (error) throw error
+
+  yScale.domain([d3.min(patients, injuryAge), d3.max(patients, injuryAge)])
 
   // join patients and create groups for each
   svg.selectAll(".patient")
     .data(patients.map(enhance))
-    .enter().append("g").classed("patient", true)
+    .enter()
+    .append("g")
+      .classed("patient", true)
+      .attr("transform", patient => translate(width / 2, y(patient)))
+
 })
