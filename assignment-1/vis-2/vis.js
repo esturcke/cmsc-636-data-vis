1e2fd86d988f9661411ba5abd5b032b8abe8178e
Erik J. Sturcke <erik@sturcke.co>
2016-09-16 00:42:41 -0400

    Change y-axis to index ordering form young to old

--- assignment-1/vis-2/vis.js
+++ assignment-1/vis-2/vis.js
@@ -3,7 +3,7 @@
 /* global d3 */
 
 // patient data source
-const data = "https://gist.githubusercontent.com/esturcke/2c0a1dcfa6bce8e37f697e8525c814c2/raw/afe880efa6221efb896762c60c86aba4aa47a41e/ehr.json"
+const data = "https://gist.githubusercontent.com/esturcke/2c0a1dcfa6bce8e37f697e8525c814c2/raw/17efba6b3c7d7831fd4cbfaa18a25b54b29299e1/ehr.json"
 
 // time conversion
 const msInYears = t => t / 1000 / 3600 / 24 / 365.25
@@ -12,9 +12,10 @@ const msInYears = t => t / 1000 / 3600 / 24 / 365.25
 const injuryAge       = ({ patient }) => patient.injury.age
 const encounterAge    = ({ encounter }) => encounter.age
 const yearsFromInjury = ({ patient : { injury }, encounter }) => msInYears(encounter.date - injury.date)
+const index           = ({ patient : { i } }) => i
 
 // SVG area dimensions
-const area   = [960, 500]
+const area   = [960, 960]
 const margin = { top : 40, right : 40, bottom : 100, left : 60 }
 const height = area[1] - margin.top  - margin.bottom
 const width  = area[0] - margin.left - margin.right
@@ -23,7 +24,7 @@ const width  = area[0] - margin.left - margin.right
 const x = d3.scaleLinear().range([0, width]).nice()
 const y = d3.scaleLinear().range([height, 0]).nice()
 
-const translate = (x, y) => `translate(${x}, ${y})`
+const translate = (x, y) => `translate(${x} ${y})`
 const to        = (xVal, yVal) => datum => translate(x(xVal(datum)), y(yVal(datum)))
 
 // setup SVG area to draw on
@@ -41,17 +42,12 @@ d3.json(data, (error, data) => {
     d3.min(data, ({ patient }) => d3.min(patient.encounters, encounter => yearsFromInjury({ patient, encounter }))),
     d3.max(data, ({ patient }) => d3.max(patient.encounters, encounter => yearsFromInjury({ patient, encounter }))),
   ])
-  y.domain([0, d3.max(data, injuryAge)])
+  y.domain([d3.min(data, index), d3.max(data, index)])
 
   // set up axes
-  svg.append("g").attrs({ transform : translate(0, height) }).call(d3.axisBottom(x))
+  svg.append("g").attrs({ transform : translate(0, height + 10) }).call(d3.axisBottom(x))
   svg.append("g").attrs({ transform : translate(-5, 0) }).call(d3.axisLeft(y))
 
-function byNum(a,b) {
-    return a - b;
-}
-  console.log(data.map(({ patient }) => patient.injury.age).sort(byNum))
-  console.log(data.map(({ patient }) => patient.injury.age).sort(byNum).length)
   // join patients and create groups for each
   const patients = svg.selectAll(".patient")
     .data(data)
@@ -68,6 +64,6 @@ function byNum(a,b) {
       class     : "encounter",
       r         : 2,
       opacity   : 0.5,
-      transform : to(yearsFromInjury, injuryAge),
+      transform : to(yearsFromInjury, index),
     })
 })
