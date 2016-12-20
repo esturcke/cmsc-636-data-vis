50bfc65c5adbad97319496ef1cfde9d0af3643d4
Erik J. Sturcke <erik@sturcke.co>
2016-12-20 04:30:33 -0500

    Sort trajectories

--- assignment-5/tools/trajectories.js
+++ assignment-5/tools/trajectories.js
@@ -1,7 +1,7 @@
-import { flow, map } from "lodash/fp"
-import { merge }     from "lodash"
-import trajectories  from "~/lib/trajectories"
-import product       from "~/lib/cartesian-product"
+import { flow, map, mapValues } from "lodash/fp"
+import { merge }                from "lodash"
+import trajectories             from "~/lib/trajectories"
+import product                  from "~/lib/cartesian-product"
 
 const patients = require("../data/ehr.json")
 const symptoms = [
@@ -21,9 +21,12 @@ const symptoms = [
   "endocrine",
 ]
 
+const byIndex = (a, b) => a.i[1] - b.i[1] || a.i[0] - b.i[0]
+
 const sequences       = product(symptoms, symptoms).filter(([from, to]) => from !== to)
 const allTrajectories = flow(
   map(([from, to]) => trajectories(from, to)(patients)),
   results => merge(...results),
+  mapValues(({ trajectories, ...rest }) => ({ ...rest, trajectories : trajectories.sort(byIndex) }))
 )(sequences)
 console.log(JSON.stringify(allTrajectories, null, 2))
