c90da7bccf39322a4af20974a97ccb47dbeb0c4a
Erik J. Sturcke <erik@sturcke.co>
2016-12-20 02:44:57 -0500

    Format trajectories

--- assignment-5/src/lib/trajectories.js
+++ assignment-5/src/lib/trajectories.js
@@ -10,11 +10,19 @@ const consecutiveDuplicate = (from, to) => (encounter, i, encounters) => encount
   ? encounters[i + 1] && encounters[i + 1][to]   // keep the last for `from`, but always discard if at the end
   : encounters[i - 1] && encounters[i - 1][from] // keep the first for `to`, but always discard if at the beginning
 
+const pairAsTrajectory = (from, to) => ([encounterFrom, encounterTo]) => ({
+  trjectory : `${from}-${to}`,
+  id        : [encounterFrom.id, encounterTo.id],
+  date      : [encounterFrom.date, encounterTo.date],
+  i         : [encounterFrom.i, encounterTo.i],
+})
+
 const fromEncounters = (from, to) => flow(
   filter(encounter => encounter[from] || encounter[to]), // only consider `from` or `to`
   flatMap(duplicateIfBoth(from, to)),                    // duplicate encounters that have both with `to` first
   filter(consecutiveDuplicate(from, to)),                // remove consecutive duplicates
   chunk(2),                                              // chunk alternating encounters that are now a from → to
+  map(pairAsTrajectory(from, to)),                       // format the trjectory
 )
 
 const trajectories = (from, to) => flow(
