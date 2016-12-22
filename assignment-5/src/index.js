d4ecdea0740c1a297a8910044f2eeb30eeff3d33
Erik J. Sturcke <erik@sturcke.co>
2016-12-22 01:16:03 -0500

    Add global.scss

--- assignment-5/src/index.js
+++ assignment-5/src/index.js
@@ -2,11 +2,8 @@ import React      from "react"
 import { render } from "react-dom"
 import App        from "~/components/App"
 
-import "bootstrap/dist/css/bootstrap.min.css"
 import "minireset.css/minireset.sass"
 import "@typopro/web-fira-sans/TypoPRO-FiraSans.css"
-import "react-virtualized/styles.css"
-import "react-select/dist/react-select.css"
-import "~/styles/global.css"
+import "~/styles/global.scss"
 
 render(<App/>, document.getElementById("root"))
