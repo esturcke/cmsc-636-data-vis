412d2ee2cf12d6e8c3647a7226eae4c6b2c8beeb
Erik J. Sturcke <erik@sturcke.co>
2016-12-23 01:51:22 -0500

    Define trajectory prop type

--- assignment-5/src/lib/propTypes.js
+++ assignment-5/src/lib/propTypes.js
@@ -1,6 +1,14 @@
 import { PropTypes as T } from "react"
 
+const trajectory = T.shape({
+  trajectory : T.string.isRequired,
+  id         : T.arrayOf(T.string.isRequired).isRequired,
+  i          : T.arrayOf(T.number.isRequired).isRequired,
+  date       : T.arrayOf(T.number.isRequired).isRequired,
+})
+
 const propTypes = {
+  trajectory,
   ...T,
 }
 
